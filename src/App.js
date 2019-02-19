c4ec566deeee2bde972230deb85e531c3153d58f
nahed20 <n.add55@hotmail.com>
2019-02-19 16:36:21 +0300

    To Do List

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListItem from './ListItem'
+
+
+class App extends Component {
+  //set state to store data enterby user 
+  state = {
+
+    formData: {
+      item: '',
+    },
+    list: []
+
+  }
+
+  //handel change data 
+  hadelChange = (e) => {
+    this.setState({
+      formData: {
+        item: e.target.value
+      }
+    })
+  }
+
+  clickSubmit = (event) => {
+    event.preventDefault();
+    //
+    let newItem = this.state.formData
+    const copyArray = this.state.list.slice(0)
+    //add a new data to the array list
+    newItem.item = this.state.formData.item;
+    //add the new list data to the array
+
+    //use the state with our new array copy 
+
+    if (newItem.item !== "") {
+      copyArray.push(newItem);
+      this.setState({
+
+        list: copyArray,
+        formData: {
+          item: ""
+        }
+      })
+    }
+
+  }
+  clearList = () => {
+    this.setState({
+      list: []
+    })
+  }
+  render() {
+    const lists = this.state.list.map(item => <ListItem item={item.item} />)
+    return (
+
+      <div className="App">
+        <form onSubmit={this.clickSubmit} className="style">
+          <h1 className="set"> List Items </h1>
+
+          <input type="text" value={this.state.formData.item} onChange={this.hadelChange} className="setButtom" />
+          <button type="submit" className="button1">Submit</button>
+          <button onClick={this.clearList} className="button2">Clear List</button>
+        </form>
+        <ul>
+          {lists}
+        </ul>
+
+      </div>
+
+    )
+
+  }
+
+}
+
+export default App;
---
+++ src/ListItem.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class List extends Component {
+    state =
+        {
+            compeleted: false,
+            style: { textDecorationLine: "none" }
+        }
+
+
+    selected = () => {
+        if (this.state.compeleted) {
+            this.setState({ compeleted: false, style: { textDecorationLine: "none" } })
+        }
+        else {
+            this.setState({ compeleted: true, style: { textDecorationLine: "line-through" } })
+        }
+    }
+
+    render() {
+        return (
+            <li style={this.state.style} >
+                <span onClick={this.selected} >{this.props.item}</span>
+            </li>
+
+
+        )
+    }
+}
+
+export default List
