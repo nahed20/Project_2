03419a9c70f23941ba4064a2783839b9f89d60e0
nahed20 <n.add55@hotmail.com>
2019-02-20 16:06:06 +0300

    Updates

---
+++ precache-manifest.5ccf7be1a52cb35c2ae5024afea1ca49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Project_2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "66760616dd7786202566",
+    "url": "/Project_2/static/js/runtime~main.66760616.js"
+  },
+  {
+    "revision": "c99dccd4d3819f593f63",
+    "url": "/Project_2/static/js/main.c99dccd4.chunk.js"
+  },
+  {
+    "revision": "1b57c33a405a1b38358e",
+    "url": "/Project_2/static/js/2.1b57c33a.chunk.js"
+  },
+  {
+    "revision": "c99dccd4d3819f593f63",
+    "url": "/Project_2/static/css/main.044c15fe.chunk.css"
+  },
+  {
+    "revision": "1b57c33a405a1b38358e",
+    "url": "/Project_2/static/css/2.cef9d078.chunk.css"
+  },
+  {
+    "revision": "52995a88643abbad2dfa60e901e6718b",
+    "url": "/Project_2/index.html"
+  }
+];
